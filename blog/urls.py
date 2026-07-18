c445552edd139b9d53a96e39d48b9e1a5c23a173
Qu <Quanvle@live.dk>
2026-07-18 11:57:46 +0200

    57. Adding urls and views

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+﻿from django.urls import path
+
+from blog.views import index, posts, post
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("posts", posts , name="posts"),
+    path('posts/<slug:slug>', post , name="post"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "posts/index.html")
+
+def post(request, slug):
+    return render(request, "posts/post.html")
+
+def posts(request):
+    return render(request, "posts/posts.html")
+
