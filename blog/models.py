435053b400152760be0df3016b3de47e192704e9
Qu <Quanvle@live.dk>
2026-07-27 01:30:22 +0200

    Add the entities

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.caption
+
+
+class Post(models.Model):
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to="blog/images")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateField()
+    title = models.CharField(max_length=200)
+    excerpt = models.TextField()
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    tags = models.ManyToManyField(Tag, related_name="posts")
+
+    def __str__(self):
+        return self.title
