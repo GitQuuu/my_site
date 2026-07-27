b7d7960782b247a8e933bd8a29769c19bc31c1d7
Qu <Quanvle@live.dk>
2026-07-27 08:35:31 +0200

    Alter the datatype of image property on Post entity

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Tag(models.Model):
 
 class Post(models.Model):
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to="blog/images")
+    image = models.ImageField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     date = models.DateField()
     title = models.CharField(max_length=200)
