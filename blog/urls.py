64447c21bec5bdb9dfc454bb5e7ec2b9ca137150
Qu <QKL@blackcapitaltechnology.com>
2026-08-10 01:55:25 +0200

    Converting views to class based

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,9 @@
 ﻿from django.urls import path
 
 from blog import views
-from blog.views import posts, post
 
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("blogs", views.AllPostsView.as_view() , name="all-posts"),
-    path('blogs/<slug:slug>', post , name="post-detail-page"),
+    path('blogs/<slug:slug>', views.SinglePostView.as_view() , name="post-detail-page"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
+from multiprocessing import context
+
 from django.shortcuts import get_object_or_404, render
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from blog.models import Post
@@ -17,23 +20,18 @@ class StartingPageView(ListView):
         return data
 
 
+class SinglePostView(DetailView):
+    model = Post
+    template_name = "blogs/post-detail.html"
 
-def post(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blogs/post-detail.html", {
-        "post": identified_post,
-        "post_tags" : identified_post.tags.all()
-    } )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
 
 class AllPostsView(ListView):
     model = Post
     template_name = "blogs/all-posts.html"
     context_object_name = "all_posts"
     ordering = ("-date",)
-
-
-def posts(request):
-    return render(request, "blogs/all-posts.html", {
-        "all_posts": Post.objects.all().order_by("-date")
-    })
 
