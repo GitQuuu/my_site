d05ffa0c4e788d91592e32fd27c38c53914b975d
Qu <QKL@blackcapitaltechnology.com>
2026-08-10 02:17:54 +0200

    adding comment model

--- blog/models.py
+++ blog/models.py
@@ -28,5 +28,13 @@ class Post(models.Model):
     content = models.TextField(validators=[MinLengthValidator(10)])
     tags = models.ManyToManyField(Tag, related_name="posts")
 
+
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField(max_length=100)
+    text = models.TextField(max_length=500)
+    date = models.DateField()
